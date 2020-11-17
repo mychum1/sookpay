97e2dc38687c78181697101928c2d0b7d5f2adfc
mychum1 <want5617@gmail.com>
2020-11-17 22:43:24 +0900

    initialize

---
+++ src/main/java/com/mychum1/sookpay/SookpayApplication.java
@@ -0,0 +1,13 @@
+package com.mychum1.sookpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SookpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SookpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mychum1/sookpay/SookpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mychum1.sookpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SookpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
